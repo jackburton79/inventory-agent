64b695d85d28b3bd294fc6bfd1aa4514ed5dd563
JackBurton79 <stefano.ceccherini@gmail.com>
2026-07-08 10:25:25 +0200

    civetweb: don't compile useless stuff

--- objects.mk
+++ objects.mk
@@ -5,6 +5,10 @@ LIBS := -lz -lcrypto -lssl
 CFLAGS := -fPIE -O2 -Wall \
 	-DUSE_SSL \
 	-DOPENSSL_API_3_0 \
+	-DNO_CGI \
+	-DNO_LUA \
+	-DNO_DUKTAPE \
+	-DNO_WEBSOCKET \
 	-I./ \
 	-Ilibs/ \
 	-Ilibs/civetweb-1.16/include \
