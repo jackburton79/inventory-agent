b0a55a33e10b42cd4b8f3ed7b70114a953eb64f7
JackBurton79 <stefano.ceccherini@gmail.com>
2026-07-13 14:08:11 +0200

    Remove field from todo

--- Inventory.cpp
+++ Inventory.cpp
@@ -546,8 +546,7 @@ Inventory::_AddOperatingSystemInfo()
 	os->LinkEndChild(fqdn);
 
 #if 0
-	// TODO: Impelement these
-	<BOOT_TIME>2024-03-26 07:58:04</BOOT_TIME>
+	// TODO: Implement these
 	<FULL_NAME>
 	<HOSTID>
 	<INSTALL_DATE>
