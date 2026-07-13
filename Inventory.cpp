8684ab1a4e44bb0bc85d6ffed933c4834d7755db
JackBurton79 <stefano.ceccherini@gmail.com>
2026-07-13 14:03:35 +0200

    Also include boot time using sysinfo on linux

--- Inventory.cpp
+++ Inventory.cpp
@@ -531,6 +531,10 @@ Inventory::_AddOperatingSystemInfo()
 	kernelVersion->LinkEndChild(fDocument->NewText(gComponents["OS"].fields["release"].c_str()));
 	os->LinkEndChild(kernelVersion);
 
+	tinyxml2::XMLElement* bootTime = fDocument->NewElement("BOOT_TIME");
+	bootTime->LinkEndChild(fDocument->NewText(gComponents["OS"].fields["boot_time"].c_str()));
+	os->LinkEndChild(bootTime);
+
 	tinyxml2::XMLElement* dnsDomain = fDocument->NewElement("DNS_DOMAIN");
 	dnsDomain->LinkEndChild(fDocument->NewText(gComponents["OS"].fields["domainname"].c_str()));
 	os->LinkEndChild(dnsDomain);
